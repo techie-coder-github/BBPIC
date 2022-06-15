8ef9683f9e1e27788d4cd5bac5c56a971ed5c9b9
Vishal <vishalsinghimp000@gmail.com>
2022-06-15 16:59:25 +0530

    changed route

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
       <Routes>
       
-      <Route path='/home' element={<Main/>}/>
+      <Route path='/' element={<Main/>}/>
       <Route path='/contactus' element={<Contactus/>}/>
       
       </Routes>
